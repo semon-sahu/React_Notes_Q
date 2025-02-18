0ee518720e9d0f45cfb081d07d0efac62d85733e
Semon Sahu <111222semonsahu@gmail.com>
2025-02-18 12:32:58 +0530

    usestate

---
+++ usestate-18-02-2025/src/App.jsx
@@ -0,0 +1,45 @@
+import {useState} from "react";
+
+import './App.css'
+
+function App() {
+  let[name,setName]=useState("")
+
+  let [c,setc]=useState(0);
+
+ 
+  function demo(){
+    setName("Shazab");
+  }
+
+  function Inc(){
+    setc(c+1);
+  }
+  function Dec(){
+    if(c!=0)
+    {
+      setc(c-1);
+
+    }
+  }
+  return (
+    <>
+  <h1>My name is {name}</h1>
+  <button onClick={demo}>Tap</button>
+  <br />
+
+<h1>{c}</h1>
+  <button onClick={Dec}>-</button>
+  <button onClick={Inc}>+</button>
+
+    </>
+  )
+}
+
+export default App;
+
+
+
+//hw real dom and virtual dom
+//d/b function component class componnent  
+//what is reconcilation 
