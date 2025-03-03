57faa2eb6a8ec5b9445daf623320311d7f8fead4
Semon Sahu <111222semonsahu@gmail.com>
2025-03-03 12:42:01 +0530

    localstorage

--- FormHandling-01-03-2025/src/Form.jsx
+++ FormHandling-01-03-2025/src/Form.jsx
@@ -1,27 +1,57 @@
 import { useState } from "react";
 
-
 const Form=()=>{
 
-     let [frmdata, setfrmdata]=useState("");
+     let [frmdata, setfrmdata]=useState({
+        name:"",
+        email:"",
+        Password:""
+     })
+     
 
-    function inpuchange(e){
-        console.log(e.target.value);
-        setfrmdata(e.target.value);
 
+    function inpchange(e){
+
+        const {name,value}=e.target;
+        setfrmdata({...frmdata,[name]:value});
+
+        // console.log(e.target.value);
+        // setfrmdata(e.target.value);
+
+    }
+    function finalsubmit(e){
+        e.preventDefault();
+
+        console.log(frmdata);
+        localStorage.setItem('frmdata',JSON.stringify(frmdata));
     }
 
     return(
         <>
-         <input type="text" placeholder='Enter your name'  onChange={inpuchange}/><br />
-         <input type="text" placeholder='Enter your age'  onChange={inpuchange}/><br />
-         <input type="text" placeholder='Enter your email'  onChange={inpuchange}/><br />
-         <input type="text" placeholder='Enter your id'  onChange={inpuchange}/><br />
-         <input type="text" placeholder='Enter your address'  onChange={inpuchange}/><br />
-         <button type="submit"  onChange={inpuchange}>submit</button>
-         {frmdata}
+
+
+        <form action="" onSubmit={finalsubmit}>
+
+        <label htmlFor="">UserName:</label>
+         <input type="text" name="UserName" placeholder='Enter your name'  onChange={inpchange}/><br />
+
+        <label htmlFor="">email:</label>
+         <input type="text" name="email "placeholder='Enter your email'  onChange={inpchange}/><br />
+
+        <label htmlFor="">Password:</label>
+         <input type="text" name="Password" placeholder='Enter your Password'  onChange={inpchange}/><br />
+         <input type="submit" />
+         {/* {frmdata} */}
+         </form>
         
         </>
     )
 }
 export default Form;
+
+
+
+
+// localStorage =protocol, dom ,port ye3 hoti hai 
+// ->setitem,getItem,removeItem,clear
+// key value pair hoti hai data |string formate me hota hai
