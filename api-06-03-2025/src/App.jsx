b688a77228ad352dd2aa0f30d7614c8df5b9ca6b
Semon Sahu <111222semonsahu@gmail.com>
2025-03-11 14:51:23 +0530

    insert-update

--- api-06-03-2025/src/App.jsx
+++ api-06-03-2025/src/App.jsx
@@ -1,12 +1,14 @@
 
 import './App.css'
 import axios from 'axios'
-import { useEffect  ,useState } from 'react'
+import { use, useEffect  ,useState } from 'react'
 
 function App() {
 
   let [apidata,setApi]=useState([]);
   let [frmdata,setfrmdata]=useState([]);
+  let [shwfrm,setshw]=useState(false);
+  let [editdata,seteditdata]=useState({});
 
 
   //delete function
@@ -14,18 +16,35 @@ function del(id){
   axios.delete(`http://localhost:3000/student/${id}`)
   .then(r=>alert("Data delete successfully"))
 }
+//insert data
+function  datasubmit(e){
+  e.preventDefault()
+  axios.post('http://localhost:3000/student',frmdata)
+  .then(r=>alert("data insert"))
+}
 
-//onsert function
+//insert function
 
  function Insertinp(e){
   // axios.post('http://localhost:3000/student')
   const {name,value}=e.target
   setfrmdata({...frmdata,[name]:value})
+ }
+//edit function 
 
+function handleedit(e){
+  const {name,value}=e.target
+seteditdata({...editdata,[name]:value})
+
+}
+
+function editfinalsubmit(e){
+  e.preventDefault()
+  axios.put(`http://localhost:3000/student/${editdata.id}`,editdata)
+  .then(r=>alert("update"))
+}
   
-     }
-
-
+     
 
   useEffect(()=>{
     axios.get('http://localhost:3000/student')
@@ -44,7 +63,7 @@ function del(id){
     apidata.map((e)=>{return <h3>{e.name}<br></br>{e.age}<br></br>{e.city}<br></br>{e.profession}</h3>})
 
     } */}
-    <table border="">
+    <table border="1px">
       <tr>
         <th>ID</th>
         <th>Name</th>
@@ -52,6 +71,7 @@ function del(id){
         <th>Contsct</th>
         <th>City</th>
         <th>delete</th>
+        <th>Edit</th>
       </tr>
       {
         apidata.map((e)=>{
@@ -63,6 +83,7 @@ function del(id){
             <td>{e.city}</td>
             <td>{e.profession}</td>
             <td><button onClick={()=>del(e.id)}>Delete </button></td>
+            <td><button onClick={(setshw(true),seteditdata(e))}>Edit </button></td>
           </tr>
           </>
           )
@@ -73,9 +94,9 @@ function del(id){
     </table>
     <hr />
     <br />
-
+{/* //inster form  */}
 <h1>Instrt form</h1>
-    <form action="">
+    <form action="" onSubmit={datasubmit}>
 <label htmlFor="">Name:</label>
 <input type="text" name="name"  onChange={Insertinp} /><br />
 
@@ -91,6 +112,30 @@ function del(id){
 <input type="submit" name="" id="" />
     </form>
   
+
+
+
+
+  {/* edit form  */}
+{shwfrm && (
+  <form action=""  onSubmit={editfinalsubmit}>
+
+    <label htmlFor="">Name:</label>
+<input type="text" name="name"   value={editdata.name} onChange={handleedit} /><br />
+
+<label htmlFor="">age:</label>
+<input type="text" name="age"  value={editdata.age} onChange={handleedit} /><br />
+
+<label htmlFor="">city:</label>
+<input type="text" name="city"   value={editdata.city} onChange={handleedit} /><br />
+
+<label htmlFor="">profession:</label>
+<input type="text" name="profession"   value={editdata.profession} onChange={handleedit} /><br />
+
+<input type="submit"  />
+
+  </form>
+  )}
     </>
   )
 }
